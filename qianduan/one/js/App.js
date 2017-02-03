35333745eae7bb4f7e828af762f28f48415c06e5
th720309 <646595546@qq.com>
2017-02-03 19:14:09 +0800

    header push

---
+++ qianduan/one/js/App.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tianhao on 17-2-3.
+ */
+var appDaily = {
+    bindToggleButton: function() {
+        var btn = document.querySelector('.menu-toggle');
+        var nav = document.querySelector('nav');
+
+        btn.addEventListener('click', function() {
+            var c = nav.getAttribute('class') || '';
+
+            if (c.indexOf('show-force') !== -1) {
+                nav.setAttribute('class', c.replace(/show-force/, '').trim());
+            } else {
+                nav.setAttribute('class', (c + ' show-force').trim());
+            }
+        });
+    }
+};
+
+appDaily.bindToggleButton();
